4ac2ac388cb58dc5d8488ab5d1065b087850b9c1
The-Techno-bug <70822971+The-Techno-bug@users.noreply.github.com>
2021-01-01 17:39:24 -0500

    Delete script.js

--- the-techno-bug/.js/script.js
+++
@@ -1,9 +0,0 @@
-$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 1000); 
-});
-function removeLoader(){
-  $( "#loadingDiv" ).fadeOut(500, function() {
-  $( "#loadingDiv" ).remove();
-});  
-}
